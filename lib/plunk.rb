91291c9ad2eb6ca212312d20e62baeb5d4ccb03a
Jamil Bou Kheir <jamilboukheir@me.com>
2013-12-13 11:29:10 -0800

    size should be an option

--- lib/plunk.rb
+++ lib/plunk.rb
@@ -8,18 +8,18 @@ require 'plunk/result_set'
 module Plunk
   class << self
     attr_accessor :elasticsearch_options, :elasticsearch_client,
-      :parser, :transformer
+      :parser, :transformer, :max_number_of_hits
   end
 
   def self.configure(&block)
     class_eval(&block)
-    initialize_elasticsearch
     initialize_parser
     initialize_transformer
+    initialize_elasticsearch
   end
 
   def self.initialize_elasticsearch
-    self.elasticsearch_client ||= Elasticsearch::Client.new elasticsearch_options
+    self.elasticsearch_client ||= Elasticsearch::Client.new(elasticsearch_options)
   end
 
   def self.initialize_parser
@@ -31,8 +31,6 @@ module Plunk
   end
 
   def self.search(query_string)
-    parsed = parser.parse query_string
-    result_set = transformer.apply parsed
-    result_set.eval
+    transformer.apply(parser.parse(query_string)).eval
   end
 end
