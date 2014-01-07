3c56e09359cca69b0c42df57d958af5d5d80e715
Roman Heinrich <roman.heinrich@gmail.com>
2014-01-07 14:51:59 +0100

    add helper module

--- lib/plunk/transformer.rb
+++ lib/plunk/transformer.rb
@@ -2,6 +2,35 @@ require 'parslet'
 require 'active_support/core_ext'
 
 module Plunk
+
+  class Helper
+    def self.time_query_to_timestamp(int_quantity, quantifier)
+      case quantifier
+      when 's'
+        int_quantity.seconds.ago
+      when 'm'
+        int_quantity.minutes.ago
+      when 'h'
+        int_quantity.hours.ago
+      when 'd'
+        int_quantity.days.ago
+      when 'w'
+        int_quantity.weeks.ago
+      end
+    end
+
+    def self.timestamp_format(time)
+      time.utc.to_datetime.iso8601(3)
+    end
+
+    def self.time_range_hash(start_time, end_time)
+      {
+        start_time: PlunkHelper.timestamp_format(start_time),
+        end_time: PlunkHelper.timestamp_format(end_time)
+      }
+    end
+  end
+
   class Transformer < Parslet::Transform
 
     rule(
