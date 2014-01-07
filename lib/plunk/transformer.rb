9223739b23e2849a2eacca89b1695e1b6f2ebefb
Roman Heinrich <roman.heinrich@gmail.com>
2014-01-07 14:52:28 +0100

    use helper module

--- lib/plunk/transformer.rb
+++ lib/plunk/transformer.rb
@@ -25,8 +25,8 @@ module Plunk
 
     def self.time_range_hash(start_time, end_time)
       {
-        start_time: PlunkHelper.timestamp_format(start_time),
-        end_time: PlunkHelper.timestamp_format(end_time)
+        start_time: Plunk::Helper.timestamp_format(start_time),
+        end_time: Plunk::Helper.timestamp_format(end_time)
       }
     end
   end
@@ -46,40 +46,20 @@ module Plunk
       }) do
 
       int_quantity = quantity.to_s.to_i
-
-      start_time =
-        case quantifier
-        when 's'
-          int_quantity.seconds.ago
-        when 'm'
-          int_quantity.minutes.ago
-        when 'h'
-          int_quantity.hours.ago
-        when 'd'
-          int_quantity.days.ago
-        when 'w'
-          int_quantity.weeks.ago
-        end
-
-      end_time = Time.now
+      start_time   = Plunk::Helper.time_query_to_timestamp(int_quantity, quantifier)
+      end_time     = Time.now
 
       # recursively apply nested query
       result_set = Plunk::Transformer.new.apply(initial_query)
 
       json = JSON.parse result_set.eval
-      values = Plunk::Utils.extract_values json, extractors.to_s.split(',') 
+      values = Plunk::Utils.extract_values json, extractors.to_s.split(',')
 
+      result_set_params = Plunk::Helper.time_range_hash(start_time, end_time)
       if values.empty?
-        ResultSet.new(
-          start_time: start_time.utc.to_datetime.iso8601(3),
-          end_time: end_time.utc.to_datetime.iso8601(3))
-          
-      else
-        ResultSet.new(
-          query_string: "#{field}:(#{values.uniq.join(' OR ')})",
-          start_time: start_time.utc.to_datetime.iso8601(3),
-          end_time: end_time.utc.to_datetime.iso8601(3))
+        result_set_params.merge!(query_string: "#{field}:(#{values.uniq.join(' OR ')})",)
       end
+      Plunk::ResultSet.new(result_set_params)
     end
 
     rule(match: simple(:value)) do
@@ -98,7 +78,7 @@ module Plunk
       result_set = Transformer.new.apply(initial_query)
 
       json = JSON.parse result_set.eval
-      values = Utils.extract_values json, extractors.to_s.split(',') 
+      values = Utils.extract_values json, extractors.to_s.split(',')
 
       if values.empty?
         ResultSet.new
@@ -118,26 +98,11 @@ module Plunk
       }) do
 
       int_quantity = quantity.to_s.to_i
+      start_time   = Plunk::Helper.time_query_to_timestamp(int_quantity, quantifier)
+      end_time     = Time.now
 
-      start_time =
-        case quantifier
-        when 's'
-          int_quantity.seconds.ago
-        when 'm'
-          int_quantity.minutes.ago
-        when 'h'
-          int_quantity.hours.ago
-        when 'd'
-          int_quantity.days.ago
-        when 'w'
-          int_quantity.weeks.ago
-        end
-
-      end_time = Time.now
-
-      ResultSet.new(
-        start_time: start_time.utc.to_datetime.iso8601(3),
-        end_time: end_time.utc.to_datetime.iso8601(3))
+      result_set_params = Plunk::Helper.time_range_hash(start_time, end_time)
+      Plunk::ResultSet.new(result_set_params)
     end
 
     rule(
@@ -148,27 +113,12 @@ module Plunk
       }) do
 
       int_quantity = quantity.to_s.to_i
+      start_time   = Plunk::Helper.time_query_to_timestamp(int_quantity, quantifier)
+      end_time     = Time.now
 
-      start_time =
-        case quantifier
-        when 's'
-          int_quantity.seconds.ago
-        when 'm'
-          int_quantity.minutes.ago
-        when 'h'
-          int_quantity.hours.ago
-        when 'd'
-          int_quantity.days.ago
-        when 'w'
-          int_quantity.weeks.ago
-        end
-
-      end_time = Time.now
-
-      ResultSet.new(
-        query_string: result_set.query_string,
-        start_time: start_time.utc.to_datetime.iso8601(3),
-        end_time: end_time.utc.to_datetime.iso8601(3))
+      result_set_params = Plunk::Helper.time_range_hash(start_time, end_time)
+      result_set_params.merge!(query_string: result_set.query_string)
+      Plunk::ResultSet.new(result_set_params)
     end
   end
 end
