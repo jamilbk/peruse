fadc7f7fb4fa55a8ef550cf7eb3291627e00b751
Jamil Bou Kheir <jamilboukheir@me.com>
2014-02-06 15:03:08 -0800

    Properly escape regex forward slash

--- spec/regexp_spec.rb
+++ spec/regexp_spec.rb
@@ -8,10 +8,10 @@ describe 'regexp searches' do
     }}}}})
   end
 
-  pending 'should parse foo=/blah\/ foo/' do
+  it 'should parse foo=/blah\/ foo/' do
     result = @transformer.apply @parser.parse('foo=/blah\/ foo/')
     expect(result.query).to eq({query:{filtered:{query:{query_string:{
-      query: 'foo=/blah\/ foo/'
+      query: 'foo:/blah\/ foo/'
     }}}}})
   end
 end
