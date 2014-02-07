03c97a219079324b9a58b2e7095239a4c19808bc
Jamil Bou Kheir <jamilboukheir@me.com>
2014-02-07 10:35:00 -0800

    fix regex spec

--- spec/regexp_spec.rb
+++ spec/regexp_spec.rb
@@ -14,4 +14,11 @@ describe 'regexp searches' do
       query: 'foo:/blah\/ foo/'
     }}}}})
   end
+
+  it 'should parse foo=/blah\. foo/' do
+    result = @transformer.apply @parser.parse('foo=/blah\. foo/')
+    expect(result.query).to eq({query:{filtered:{query:{query_string:{
+      query: 'foo:/blah\. foo/'
+    }}}}})
+  end
 end
