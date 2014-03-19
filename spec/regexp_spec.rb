b6325a5867637a115432942f38b879f682709437
Jamil Bou Kheir <jamil@elbii.com>
2014-03-19 14:10:38 -0400

    add value-only regexp spec

--- spec/regexp_spec.rb
+++ spec/regexp_spec.rb
@@ -25,4 +25,12 @@ describe 'regexp searches' do
     )
     expect(result).to eq(expected)
   end
+
+  it 'should parse /.*User\-Agent\: Microsoft\-WebDAV.*/' do
+    result = Plunk.search '/.*User\-Agent\: Microsoft\-WebDAV.*/'
+    expected = Plunk::Helper.filter_builder(
+      Plunk::Helper.query_builder('/.*User\-Agent\: Microsoft\-WebDAV.*/')
+    )
+    expect(result).to eq(expected)
+  end
 end
