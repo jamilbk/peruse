c239044e7384836007a063f2d13b6e8451d3ad27
Jamil Bou Kheir <jamil@elbii.com>
2014-09-16 11:40:54 -0500

    add more options to plunk binstub
    closes #16

--- spec/binstub_spec.rb
+++ spec/binstub_spec.rb
@@ -1,7 +1,31 @@
+require 'open3'
+
 describe 'binstub' do
   it 'should execute a valid query from stdin' do
+    result = ""
+
+    Open3.popen3(
+      "bin/plunk",
+      "-h localhost,127.0.0.1",
+      "-s 1",
+      "-r"
+    ) do |stdin, stdout, stderr|
+      stdin.puts "last 1w"
+      stdin.close
+      result = stdout.read.chomp
+    end
+      
+    expect(result).to be_present
   end
 
   it 'should not allow invalid options' do
+    exit_status = -1 
+
+    Open3.popen3("bin/plunk", "-z") do |stdin, stdout, stderr, thread|
+      exit_status = thread.value.exitstatus
+    end
+
+
+    expect(exit_status).to eq 1
   end
 end
